dd4d66da7eeb9aaa6dfbad324725cc6a56cce79f
Hernan <hernan.bartoletti@gmail.com>
2019-09-29 16:28:23 +0200

    prioritize planets to conqueror

---
+++ hernan/hernan.cpp
@@ -0,0 +1,146 @@
+#include "hlt/hlt.hpp"
+#include "hlt/navigation.hpp"
+#include <strstream>
+#include <tuple>
+#include <cstdint>
+#include <iomanip>
+
+const hlt::Metadata metadata = hlt::initialize("Hernan");
+const hlt::PlayerId player_id = metadata.player_id;
+
+namespace utils
+{
+	namespace planets {
+
+		uint32_t getPriority(const hlt::Planet& planet)
+		{
+			uint32_t priority = 0;
+
+//			if (planet.is_alive())
+//				priority |= 0x8000000U;
+
+			if (planet.owned && planet.owner_id == player_id)
+				priority |= 0x4000000U;
+
+			if (!planet.owned)
+				priority |= 0x2000000U;
+			
+			if(!planet.is_full())
+				priority |= 0x8000000U;
+
+			double radius = planet.radius * 1E3;
+			priority |= (radius < 65535.0) ? uint16_t(radius) : 0xFFFFU;
+
+			return priority;
+		}
+
+
+		struct less_than
+		{
+			// If planet lhs is less interesting to conqueror than rhs, this method must return false
+			inline bool operator() (const hlt::Planet& lhs, const hlt::Planet& rhs)
+			{
+				return getPriority(lhs) > getPriority(rhs);
+			}
+		};
+	}
+}
+
+int main() {
+
+	const hlt::Map& initial_map = metadata.initial_map;
+
+	// We now have 1 full minute to analyse the initial map.
+	std::ostringstream initial_map_intelligence;
+	initial_map_intelligence
+		<< "width: " << initial_map.map_width
+		<< "; height: " << initial_map.map_height
+		<< "; players: " << initial_map.ship_map.size()
+		<< "; my ships: " << initial_map.ship_map.at(player_id).size()
+		<< "; planets: " << initial_map.planets.size();
+	hlt::Log::log(initial_map_intelligence.str());
+
+	std::vector<hlt::Move> moves;
+
+	std::stringstream ss;
+
+	ss << "my player_id: " << player_id << std::endl;
+	hlt::Log::log(ss.str());
+
+	for (;;)
+	{			
+		size_t planets_cnt;
+		moves.clear();
+		const hlt::Map map = hlt::in::get_map();
+
+		std::vector<hlt::Planet> planets = map.planets;
+		std::sort(planets.begin(), planets.end(), utils::planets::less_than());
+
+		std::vector<hlt::Planet>::iterator it = planets.begin();
+
+		std::stringstream ss;
+		ss << "planets:" << std::endl;
+		for (planets_cnt=0; it != planets.end(); ++it)
+		{
+			ss << std::dec
+				<< " planet_id: "  << (*it).entity_id 
+				<< "; owned: "    << (*it).owned 
+				<< "; owner_id: " << (*it).owner_id
+				<< "; is_full: " << (*it).is_full()
+				<< "; radius: " << (*it).radius
+				<< "; spots/ships: " << (*it).docking_spots << "/" << (*it).docked_ships.size() 
+				<< "; priority: " << std::hex << std::setiosflags(std::ios::showbase) << utils::planets::getPriority(*it)
+				<< std::endl;
+
+			if ((*it).is_alive())
+			{
+				++planets_cnt;
+			}
+		}
+		hlt::Log::log(ss.str());
+		
+		size_t ship_cnt = 0;
+		for (const hlt::Ship& ship : map.ships.at(player_id))
+		{
+			if (ship.docking_status != hlt::ShipDockingStatus::Undocked)
+				continue;
+
+			++ship_cnt;
+		}
+
+		size_t delta = 1 + (ship_cnt / planets_cnt);
+		it = planets.begin();
+		ship_cnt = 0;
+		for (const hlt::Ship& ship : map.ships.at(player_id))
+		{
+			if (ship.docking_status != hlt::ShipDockingStatus::Undocked)
+				continue;
+
+			++ship_cnt;
+
+			if (ship.can_dock(*it) && !(*it).is_full() )
+			{
+				moves.push_back(hlt::Move::dock(ship.entity_id, (*it).entity_id));
+			}
+			else
+			{
+				const hlt::possibly<hlt::Move> move = hlt::navigation::navigate_ship_to_dock(map, ship, *it, hlt::constants::MAX_SPEED);
+
+				if (move.second) 
+				{
+					moves.push_back(move.first);
+				}
+			}
+
+			if ((ship_cnt % delta) == 0)
+			{
+				++it;
+			}
+		}
+
+		if (!hlt::out::send_moves(moves)) {
+			hlt::Log::log("send_moves failed; exiting");
+			break;
+		}
+	}
+}
---
+++ run_game.bat
@@ -0,0 +1,3 @@
+rem call make.bat
+
+.\halite.exe -d "240 160" "test01\Debug\default.exe" "hernan\Debug\hernan.exe"
