466ad7c23255373c5cdd17556574bd16224331c5
Hernan <hernan.bartoletti@gmail.com>
2019-10-01 23:56:51 +0200

    - Update priority calculation on each round for each ship

--- hernan/hernan.cpp
+++ hernan/hernan.cpp
@@ -4,6 +4,7 @@
 #include <tuple>
 #include <cstdint>
 #include <iomanip>
+#include <utility>
 
 const hlt::Metadata metadata = hlt::initialize("Hernan");
 const hlt::PlayerId player_id = metadata.player_id;
@@ -12,22 +13,27 @@ namespace utils
 {
 	namespace planets {
 
-		uint32_t getPriority(const hlt::Planet& planet)
+		uint32_t getPriority(const hlt::Planet& planet, double distance = 0.0)
 		{
 			static const uint32_t STATE_MASK = 0x07;
-			static const uint8_t STATE_MASK_SHIFT = 29;
+			static const uint8_t  STATE_MASK_SHIFT = 29;
+			static const uint32_t RADIUS_MASK = 0xFFFF;
+			static const uint8_t  RADIUS_MASK_SHIFT = 10;
+			static const uint32_t DISTANCE_MASK = 0x3FF;
+			static const uint8_t  DISTANCE_MASK_SHIFT = 0;
 
 			typedef enum
-			{	UNKNOWN = 0
-			,	MINE_AND_FULL 
-			,	OTHER
-			,	EMPTY
-			,	MINE_AND_NOT_FULL 
+			{
+				UNKNOWN = 0
+				, MINE_AND_FULL
+				, OTHER
+				, EMPTY
+				, MINE_AND_NOT_FULL
 			} state_t;
 
 			state_t state = state_t::UNKNOWN;
 
-			uint32_t priority = 0;
+			uint32_t priority = 0, ui32;
 
 			if (planet.owned && planet.owner_id == player_id)
 			{
@@ -38,11 +44,22 @@ namespace utils
 				state = planet.owned ? OTHER : EMPTY;
 			}
 
-			priority = (STATE_MASK & state) << STATE_MASK_SHIFT;
+			ui32 = ((STATE_MASK & state) << STATE_MASK_SHIFT);
+			priority |= ui32;
 
 			double radius = planet.radius * 1E3;
-			priority |= (radius < 65535.0) ? uint16_t(radius) : 0xFFFFU;
+			ui32 = (((radius < RADIUS_MASK) ? uint32_t(radius) : RADIUS_MASK) << RADIUS_MASK_SHIFT);
+			priority |= ui32;
 
+			if (distance > 0.0)
+			{			
+				ui32 = uint32_t(distance);
+				ui32 = ((ui32 < DISTANCE_MASK) ? ui32 : DISTANCE_MASK);
+				ui32 = ~ui32;
+				ui32 &= DISTANCE_MASK;
+				ui32 <<= DISTANCE_MASK_SHIFT;
+				priority |= ui32;
+			}
 			return priority;
 		}
 
@@ -53,6 +70,11 @@ namespace utils
 			inline bool operator() (const hlt::Planet& lhs, const hlt::Planet& rhs)
 			{
 				return getPriority(lhs) > getPriority(rhs);
+			}
+
+			inline bool operator() (const std::pair<const hlt::Planet&, const hlt::Ship&>& lhs, const std::pair<const hlt::Planet&, const hlt::Ship&>& rhs)
+			{
+				return getPriority(lhs.first, lhs.first.location.get_distance_to(lhs.second.location)) > getPriority(rhs.first, rhs.first.location.get_distance_to(rhs.second.location));
 			}
 		};
 	}
@@ -128,9 +150,6 @@ int main() {
 		ss << std::dec << std::endl 
 			<< "We have " << planets_cnt << " alive planets and " << ship_cnt << " undocked ships." << std::endl;
 
-		ss << std::endl 
-			<< "Movements: " << std::endl;
-		
 		ship_cnt = 0;
 		for (const hlt::Ship& ship : map.ships.at(player_id))
 		{
@@ -139,25 +158,60 @@ int main() {
 
 			++ship_cnt;
 
-			if (ship.can_dock(*it) && !(*it).is_full() )
+			std::vector<std::pair<hlt::Planet, hlt::Ship> > planetsPerShip;
+			for (const hlt::Planet& planet : planets)
 			{
-				moves.push_back(hlt::Move::dock(ship.entity_id, (*it).entity_id));
-				ss << " docking ship " << ship.entity_id << " to planet " << (*it).entity_id << std::endl;
+				planetsPerShip.push_back(std::make_pair(planet, ship));
+			}
+			
+			std::sort(planetsPerShip.begin(), planetsPerShip.end(), utils::planets::less_than());
+			std::vector<std::pair<hlt::Planet, hlt::Ship> >::iterator it = planetsPerShip.begin();
+
+			ss << "Planets from Ship:" << std::endl;
+
+			for (; it != planetsPerShip.end(); ++it)
+			{
+				ss << std::dec
+					<< " id planet/ship: " << (*it).first.entity_id << "/" << (*it).second.entity_id
+					<< "; owned: " << (*it).first.owned
+					<< "; owner_id: " << (*it).first.owner_id
+					<< "; is_full: " << (*it).first.is_full()
+					<< "; radius: " << (*it).first.radius
+					<< "; spots/ships: " << (*it).first.docking_spots << "/" << (*it).first.docked_ships.size()
+					<< "; production current/remaining: " << (*it).first.current_production << "/" << (*it).first.remaining_production
+					<< "; distance: " << (*it).first.location.get_distance_to((*it).second.location)
+					<< "; priority: " << std::hex << std::setiosflags(std::ios::showbase) << utils::planets::getPriority((*it).first, (*it).first.location.get_distance_to((*it).second.location))
+					<< std::endl;
+			}
+
+			ss << std::endl
+				<< "Movements: " << std::endl;
+
+			it = planetsPerShip.begin();
+
+			if (ship.can_dock((*it).first) && (!(*it).first.owned||(*it).first.owner_id==player_id && !(*it).first.is_full()) )
+			{
+				moves.push_back(hlt::Move::dock(ship.entity_id, (*it).first.entity_id));
+				ss << " docking ship " << ship.entity_id << " to planet " << (*it).first.entity_id << std::endl;
 			}
 			else
 			{
-				const hlt::possibly<hlt::Move> move = hlt::navigation::navigate_ship_to_dock(map, ship, *it, hlt::constants::MAX_SPEED);
+				hlt::possibly<hlt::Move> move;
+				if((!(*it).first.owned || (*it).first.owner_id == player_id && !(*it).first.is_full()))
+				{
+					move = hlt::navigation::navigate_ship_to_dock(map, ship, (*it).first, hlt::constants::MAX_SPEED);
+				}
+				else
+				{
+					move = hlt::navigation::navigate_ship_towards_target(map, ship, (*it).first.location, hlt::constants::MAX_SPEED, true, 10, 90);
+				}
+				
 
 				if (move.second) 
 				{
 					moves.push_back(move.first);
-					ss << " moving ship " << ship.entity_id << " to planet " << (*it).entity_id << std::endl;
+					ss << " moving ship " << ship.entity_id << " to planet " << (*it).first.entity_id << std::endl;
 				}
-			}
-
-			if ((ship_cnt % delta) == 0)
-			{
-//				++it;
 			}
 		}
 
